3ad1bc131c611b6806aa6e2c93781432be19f002
manik0493 <manik.narang@yahoo.com>
2016-11-14 17:00:17 +0530

    added automtn thru npm scripts. transpiling thru babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var path= require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open  from 'open';
 
 var port = 3000;
 var app = express();
