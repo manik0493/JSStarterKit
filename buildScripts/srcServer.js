a79c730fe69169002f4ddb1585b673fd4be45259
manik0493 <manik.narang@yahoo.com>
2016-11-15 17:09:26 +0530

    linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,7 @@ import open  from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-
+/* eslint-disable no-console */
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import numeral from 'numeral';
 
+import './index.css';
+import numeral from 'numeral';
+debugger;
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`);
